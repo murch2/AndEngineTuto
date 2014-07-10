729054b67b983d13b6633d973f88865ec0a812a0
Rodrigo Duarte Louro <digao.louro@gmail.com>
2014-07-10 15:19:20 -0300

    colocando a biblioteca de Fisica da Branch certa

--- src/com/scenes/GameScene.java
+++ src/com/scenes/GameScene.java
@@ -1,16 +1,32 @@
 package com.scenes;
 
+import java.io.IOException;
+
 import org.andengine.engine.camera.hud.HUD;
+import org.andengine.entity.IEntity;
+import org.andengine.entity.modifier.LoopEntityModifier;
+import org.andengine.entity.modifier.ScaleModifier;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.text.Text;
 import org.andengine.entity.text.TextOptions;
 import org.andengine.extension.physics.box2d.FixedStepPhysicsWorld;
+import org.andengine.extension.physics.box2d.PhysicsConnector;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.util.SAXUtils;
 import org.andengine.util.adt.align.HorizontalAlign;
 import org.andengine.util.adt.color.Color;
+import org.andengine.util.level.EntityLoader;
+import org.andengine.util.level.constants.LevelConstants;
+import org.andengine.util.level.simple.SimpleLevelEntityLoaderData;
 import org.andengine.util.level.simple.SimpleLevelLoader;
+import org.xml.sax.Attributes;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.manager.SceneManager;
 import com.manager.SceneManager.SceneType;
 import com.util.Constants;
@@ -81,8 +97,86 @@ public class GameScene extends BaseScene {
 		registerUpdateHandler(physicsWorld); 
 	}
 	
-	private void loadLevel(int levelID) {
-		final SimpleLevelLoader levelLoader = new SimpleLevelLoader(vbom); 
+	private void loadLevel(int levelID)
+	{
+	    final SimpleLevelLoader levelLoader = new SimpleLevelLoader(vbom);
+	    
+	    final FixtureDef FIXTURE_DEF = PhysicsFactory.createFixtureDef(0, 0.01f, 0.5f);
+	    
+	    levelLoader.registerEntityLoader(new EntityLoader<SimpleLevelEntityLoaderData>(LevelConstants.TAG_LEVEL)
+	    {
+	        public IEntity onLoadEntity(final String pEntityName, final IEntity pParent, final Attributes pAttributes, final SimpleLevelEntityLoaderData pSimpleLevelEntityLoaderData) throws IOException 
+	        {
+	            final int width = SAXUtils.getIntAttributeOrThrow(pAttributes, LevelConstants.TAG_LEVEL_ATTRIBUTE_WIDTH);
+	            final int height = SAXUtils.getIntAttributeOrThrow(pAttributes, LevelConstants.TAG_LEVEL_ATTRIBUTE_HEIGHT);
+	            
+	            // TODO later we will specify camera BOUNDS and create invisible walls
+	            // on the beginning and on the end of the level.
+
+	            return GameScene.this;
+	        }
+	    });
+	    
+	    levelLoader.registerEntityLoader(new EntityLoader<SimpleLevelEntityLoaderData>(TAG_ENTITY)
+	    {
+	        public IEntity onLoadEntity(final String pEntityName, final IEntity pParent, final Attributes pAttributes, final SimpleLevelEntityLoaderData pSimpleLevelEntityLoaderData) throws IOException
+	        {
+	            final int x = SAXUtils.getIntAttributeOrThrow(pAttributes, TAG_ENTITY_ATTRIBUTE_X);
+	            final int y = SAXUtils.getIntAttributeOrThrow(pAttributes, TAG_ENTITY_ATTRIBUTE_Y);
+	            final String type = SAXUtils.getAttributeOrThrow(pAttributes, TAG_ENTITY_ATTRIBUTE_TYPE);
+	            
+	            final Sprite levelObject;
+	            
+	            if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM1))
+	            {
+	                levelObject = new Sprite(x, y, resourcesManager.platform1_region, vbom);
+	                PhysicsFactory.createBoxBody(physicsWorld, levelObject, BodyType.StaticBody, FIXTURE_DEF).setUserData("platform1");
+	            } 
+	            else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM2))
+	            {
+	                levelObject = new Sprite(x, y, resourcesManager.platform2_region, vbom);
+	                final Body body = PhysicsFactory.createBoxBody(physicsWorld, levelObject, BodyType.StaticBody, FIXTURE_DEF);
+	                body.setUserData("platform2");
+	                physicsWorld.registerPhysicsConnector(new PhysicsConnector(levelObject, body, true, false));
+	            }
+	            else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM3))
+	            {
+	                levelObject = new Sprite(x, y, resourcesManager.platform3_region, vbom);
+	                final Body body = PhysicsFactory.createBoxBody(physicsWorld, levelObject, BodyType.StaticBody, FIXTURE_DEF);
+	                body.setUserData("platform3");
+	                physicsWorld.registerPhysicsConnector(new PhysicsConnector(levelObject, body, true, false));
+	            }
+	            else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_COIN))
+	            {
+	                levelObject = new Sprite(x, y, resourcesManager.coin_region, vbom)
+	                {
+	                    @Override
+	                    protected void onManagedUpdate(float pSecondsElapsed) 
+	                    {
+	                        super.onManagedUpdate(pSecondsElapsed);
+	                        
+	                        /** 
+	                         * TODO
+	                         * we will later check if player collide with this (coin)
+	                         * and if it does, we will increase score and hide coin
+	                         * it will be completed in next articles (after creating player code)
+	                         */
+	                    }
+	                };
+	                levelObject.registerEntityModifier(new LoopEntityModifier(new ScaleModifier(1, 1, 1.3f)));
+	            }            
+	            else
+	            {
+	                throw new IllegalArgumentException();
+	            }
+
+	            levelObject.setCullingEnabled(true);
+
+	            return levelObject;
+	        }
+	    });
+
+	    levelLoader.loadLevelFromAsset(activity.getAssets(), "level/" + levelID + ".lvl");
 	}
 	
 	
